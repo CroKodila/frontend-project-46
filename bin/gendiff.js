78dae5d65e17c4bc3212f686fd1ecce7c56bc9d2
CroKodila <kostyaantonov999@gmail.com>
2024-01-10 01:22:45 +0300

    fix: .gitignore

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,7 +4,9 @@ const program = new Command();
 
 program
   .version('1.0.0')
+  .arguments('filepath1 filepath2')
   .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format <type>', 'output format')
   .action(() => {
     console.log('Usage: gendiff [options]');
   });

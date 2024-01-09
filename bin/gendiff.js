aa89562a8b96c7c880592500ed93277ed693b14f
CroKodila <kostyaantonov999@gmail.com>
2024-01-10 02:08:06 +0300

    add: dataFromFilesParse.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,14 +1,16 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
+import { dataFromFileParse } from '../src/dataFromFileParse.js';
 const program = new Command();
 
 program
   .version('1.0.0')
-  .arguments('filepath1 filepath2')
+  .arguments('<filepath1> <filepath2>')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format <type>', 'output format')
-  .action(() => {
-    console.log('Usage: gendiff [options]');
+  .action((filepath1, filepath2, options) => {
+    const format = options.format || 'json';
+    dataFromFileParse(filepath1, filepath2, format);
   });
 
 program.parse(process.argv);

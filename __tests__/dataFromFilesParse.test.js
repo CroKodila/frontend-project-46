8e22d9eb3dfef6bf5cbe33f51b7751b86c960067
CroKodila <kostyaantonov999@gmail.com>
2024-01-10 17:45:43 +0300

    add: tests

--- __tests__/dataFromFilesParse.test.js
+++ __tests__/dataFromFilesParse.test.js
@@ -1,12 +1,21 @@
-import { dataFromFilesParse } from "../src/dataFromFilesParse.js";
-test('testing with empty files', () => { 
-    console.log = jest.fn();
-    dataFromFilesParse(path.resolve('test1file1.json'), path.resolve('test1file2.json'));
-    expect(console.log).toHaveBeenCalledWith('{\n}');
+import path from 'path';
+import { dataFromFilesParse } from '../src/dataFromFilesParse.js';
+import {jest} from '@jest/globals';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+
+test('testing with empty files', () => {
+  console.log = jest.fn();
+  dataFromFilesParse(getFixturePath('test1file1.json'), getFixturePath('test1file2.json'));
+  expect(console.log).toHaveBeenCalledWith('{\n\n}');
 });
 
-test('testing with non-empty files', () => { 
-    console.log = jest.fn();
-    dataFromFilesParse(path.resolve('test2file1.json'), path.resolve('test2file2.json'));
-    expect(console.log).toHaveBeenCalledWith('{\n+ follow: false\n  host: hexlet.io\n+ proxy: 123.234.53.22\n- timeout: 20\n+ timeout: 50\n- verbose: true\n}');
-})
+test('testing with non-empty files', () => {
+  console.log = jest.fn();
+  dataFromFilesParse(getFixturePath('test2file1.json'), getFixturePath('test2file2.json'));
+  expect(console.log).toHaveBeenCalledWith('{\n+ follow: false\n  host: hexlet.io\n+ proxy: 123.234.53.22\n- timeout: 20\n+ timeout: 50\n- verbose: true\n}');
+});
--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,15 +1,16 @@
 #!/usr/bin/env node
-import { Command } from "commander";
-import { dataFromFilesParse } from "../src/dataFromFilesParse.js";
+import { Command } from 'commander';
+import { dataFromFilesParse } from '../src/dataFromFilesParse.js';
+
 const program = new Command();
 
 program
-  .version("1.0.0")
-  .arguments("<filepath1> <filepath2>")
-  .description("Compares two configuration files and shows a difference.")
-  .option("-f, --format <type>", "output format")
+  .version('1.0.0')
+  .arguments('<filepath1> <filepath2>')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format <type>', 'output format')
   .action((filepath1, filepath2, options) => {
-    const format = options.format || "json";
+    const format = options.format || 'json';
     dataFromFilesParse(filepath1, filepath2, format);
   });
 
